41358e2e0416553d64953b6923fcf566e2326d6b
netabecker <netabec3@gmail.com>
2021-05-08 20:24:10 +0300

    start of cache class

--- Wet2/cache.cpp
+++ Wet2/cache.cpp
@@ -1,4 +1,27 @@
 //
 // Created by shell on 2021-05-08.
 //
+#include <vector>
 
+
+typedef enum {NO_WRITE_ALLOCATE, WRITE_ALLOCATE} policy;
+typedef enum {L1, L2} cache_name;
+typedef enum {DOESNT_EXIST = 0 , EXIST = 1} data_status;
+
+class cache {
+private:
+    int size_of_cache;
+    int block_size;
+    int associative_level;
+    int access_time;
+    policy cache_policy;
+public:
+    int access_count = 0;
+    std::vector< std::vector<data_status> > data;
+};
+
+class memory{
+public:
+    cache L1;
+    cache L2;
+};
