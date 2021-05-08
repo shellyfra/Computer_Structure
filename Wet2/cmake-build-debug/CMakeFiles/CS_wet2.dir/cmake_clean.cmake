b926411e79a49ce1961c9e9c2751178a0495d518
shellyfra <shellyfra@hotmail.com>
2021-05-08 19:10:20 +0300

    dded build-debug folder

---
+++ Wet2/cmake-build-debug/CMakeFiles/CS_wet2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS_wet2.dir/cacheSim.cpp.obj"
+  "CS_wet2.exe"
+  "CS_wet2.exe.manifest"
+  "CS_wet2.pdb"
+  "libCS_wet2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS_wet2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
