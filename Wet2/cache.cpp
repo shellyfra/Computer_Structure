0290da2c425a5e0d618cce70c0c280bc32d7b997
shellyfra <shellyfra@hotmail.com>
2021-05-10 14:10:59 +0300

    moved too different file

--- Wet2/cache.cpp
+++ Wet2/cache.cpp
@@ -2,48 +2,52 @@
 // Created by shell on 2021-05-08.
 //
 #include <vector>
-#include "math.h"
+#include <cmath>
 
 
 typedef enum {NO_WRITE_ALLOCATE, WRITE_ALLOCATE} policy;
 typedef enum {L1, L2} cache_name;
 typedef enum {DOESNT_EXIST = 0 , EXIST = 1} data_status;
 
-class Cache {
-public:
-    unsigned int size_of_cache;
-    unsigned int block_size;
-    unsigned int associative_level;
-    unsigned int num_of_rows; // size_of_cache /(block_size * associative_level)
-    int access_count = 0;
-    std::vector< std::vector<data_status> > data;
-    Cache(unsigned int cache_size, unsigned int block_size, unsigned int associative_level): size_of_cache(cache_size),
-        block_size(block_size), associative_level(associative_level){
-        num_of_rows = size_of_cache /(block_size * associative_level);
-        data.resize(num_of_rows, std::vector<data_status>(associative_level,DOESNT_EXIST));
-    }
-};
-
-class Memory {
-public:
-    Cache* L1_cache;
-    Cache* L2_cache;
-    policy cache_policy;
-    int access_count_L1 = 0;
-    int access_count_L2 = 0;
-    int access_count_mem = 0;
-    unsigned int dram_cycles;
-    unsigned int L1_cycles;
-    unsigned int L2_cycles;
-    Memory(unsigned MemCyc,unsigned BSize, unsigned L1Size, unsigned L2Size, unsigned L1Assoc,
-           unsigned L2Assoc, unsigned L1Cyc, unsigned L2Cyc, unsigned WrAlloc) : dram_cycles(MemCyc), L1_cycles(L1Cyc),
-           L2_cycles(L2Cyc){ // c'tor
-        // TODO : do pow(2, x) for : block_size, cache_size, associatvie
-        L1_cache = new Cache(pow(2,L1Size), pow(2, BSize), pow(2,L1Assoc));
-        L2_cache = new Cache(pow(2,L2Size), pow(2, BSize), pow(2,L2Assoc));
-    }
-    ~Memory() {
-        delete L1_cache;
-        delete L2_cache;
-    }
-};
+//class Cache {
+//public:
+//    unsigned int size_of_cache;
+//    unsigned int block_size;
+//    unsigned int associative_level;
+//    unsigned int num_of_rows; // size_of_cache /(block_size * associative_level)
+//    int access_count = 0;
+//    std::vector< std::vector<data_status> > data;
+//    Cache(unsigned int cache_size, unsigned int block_size, unsigned int associative_level): size_of_cache(cache_size),
+//        block_size(block_size), associative_level(associative_level){
+//        num_of_rows = size_of_cache /(block_size * associative_level);
+//        data.resize(num_of_rows, std::vector<data_status>(associative_level,DOESNT_EXIST));
+//    }
+//    ~Cache() = default;
+//    Cache(Cache& other) = default;
+//};
+//
+//class Memory {
+//public:
+//    Cache* L1_cache;
+//    Cache* L2_cache;
+//    policy cache_policy;
+//    int access_count_L1 = 0;
+//    int access_count_L2 = 0;
+//    int access_count_mem = 0;
+//    unsigned int dram_cycles;
+//    unsigned int L1_cycles;
+//    unsigned int L2_cycles;
+//    Memory(unsigned MemCyc,unsigned BSize, unsigned L1Size, unsigned L2Size, unsigned L1Assoc,
+//           unsigned L2Assoc, unsigned L1Cyc, unsigned L2Cyc, unsigned WrAlloc) : dram_cycles(MemCyc), L1_cycles(L1Cyc),
+//           L2_cycles(L2Cyc){ // c'tor
+//        // TODO : do pow(2, x) for : block_size, cache_size, associatvie
+//        L1_cache = new Cache(pow(2,L1Size), pow(2, BSize), pow(2,L1Assoc));
+//        L2_cache = new Cache(pow(2,L2Size), pow(2, BSize), pow(2,L2Assoc));
+//        cache_policy = WrAlloc ? WRITE_ALLOCATE : NO_WRITE_ALLOCATE;
+//    }
+//    ~Memory() {
+//        delete L1_cache;
+//        delete L2_cache;
+//    }
+//    Memory(Memory& other) = default;
+//};
--- Wet2/cacheSim.cpp
+++ Wet2/cacheSim.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
+#include <cmath>
 
 using std::FILE;
 using std::string;
@@ -12,8 +14,53 @@ using std::endl;
 using std::cerr;
 using std::ifstream;
 using std::stringstream;
-class memory;
-class cache;
+
+typedef enum {NO_WRITE_ALLOCATE, WRITE_ALLOCATE} policy;
+typedef enum {L1, L2} cache_name;
+typedef enum {DOESNT_EXIST = 0 , EXIST = 1} data_status;
+
+class Cache {
+public:
+    unsigned int size_of_cache;
+    unsigned int block_size;
+    unsigned int associative_level;
+    unsigned int num_of_rows; // size_of_cache /(block_size * associative_level)
+    int access_count = 0;
+    std::vector< std::vector<data_status> > data;
+    Cache(unsigned int cache_size, unsigned int block_size, unsigned int associative_level): size_of_cache(cache_size),
+                                                                                             block_size(block_size), associative_level(associative_level){
+        num_of_rows = size_of_cache /(block_size * associative_level);
+        data.resize(num_of_rows, std::vector<data_status>(associative_level,DOESNT_EXIST));
+    }
+    ~Cache() = default;
+    Cache(Cache& other) = default;
+};
+
+class Memory {
+public:
+    Cache* L1_cache;
+    Cache* L2_cache;
+    policy cache_policy;
+    int access_count_L1 = 0;
+    int access_count_L2 = 0;
+    int access_count_mem = 0;
+    unsigned int dram_cycles;
+    unsigned int L1_cycles;
+    unsigned int L2_cycles;
+    Memory(unsigned MemCyc,unsigned BSize, unsigned L1Size, unsigned L2Size, unsigned L1Assoc,
+           unsigned L2Assoc, unsigned L1Cyc, unsigned L2Cyc, unsigned WrAlloc) : dram_cycles(MemCyc), L1_cycles(L1Cyc),
+                                                                                 L2_cycles(L2Cyc){ // c'tor
+        // TODO : do pow(2, x) for : block_size, cache_size, associatvie
+        L1_cache = new Cache(pow(2,L1Size), pow(2, BSize), pow(2,L1Assoc));
+        L2_cache = new Cache(pow(2,L2Size), pow(2, BSize), pow(2,L2Assoc));
+        cache_policy = WrAlloc ? WRITE_ALLOCATE : NO_WRITE_ALLOCATE;
+    }
+    ~Memory() {
+        delete L1_cache;
+        delete L2_cache;
+    }
+    Memory(Memory& other) = default;
+};
 
 int main(int argc, char **argv) {
 
@@ -93,8 +140,8 @@ int main(int argc, char **argv) {
 
 	}
 
-	memory cpu_mem(MemCyc, BSize, L1Size, L2Size, L1Assoc, L2Assoc, L1Cyc, L2Cyc,WrAlloc); // TODO : add c'tor
-	cpu_mem.calc_operation(num, operation);
+	Memory* cpu_mem = new Memory(MemCyc,BSize, L1Size, L2Size, L1Assoc, L2Assoc, L1Cyc, L2Cyc,WrAlloc );
+	cpu_mem->calc_operation(num, operation);
 	double L1MissRate;
 	double L2MissRate;
 	double avgAccTime;
