364c28208f5713ce7bb194786faaf617240619a0
shellyfra <shellyfra@hotmail.com>
2021-05-10 13:57:03 +0300

    added c'tor and d'tor

--- Wet2/cache.cpp
+++ Wet2/cache.cpp
@@ -2,31 +2,48 @@
 // Created by shell on 2021-05-08.
 //
 #include <vector>
+#include "math.h"
 
 
 typedef enum {NO_WRITE_ALLOCATE, WRITE_ALLOCATE} policy;
 typedef enum {L1, L2} cache_name;
 typedef enum {DOESNT_EXIST = 0 , EXIST = 1} data_status;
 
-class cache {
-private:
-    int size_of_cache;
-    int block_size;
-    int associative_level;
-    int access_time;
-    policy cache_policy;
-    int num_of_rows; // size_of_chace /(block_size * associative_level)
+class Cache {
 public:
+    unsigned int size_of_cache;
+    unsigned int block_size;
+    unsigned int associative_level;
+    unsigned int num_of_rows; // size_of_cache /(block_size * associative_level)
     int access_count = 0;
     std::vector< std::vector<data_status> > data;
+    Cache(unsigned int cache_size, unsigned int block_size, unsigned int associative_level): size_of_cache(cache_size),
+        block_size(block_size), associative_level(associative_level){
+        num_of_rows = size_of_cache /(block_size * associative_level);
+        data.resize(num_of_rows, std::vector<data_status>(associative_level,DOESNT_EXIST));
+    }
 };
 
-class memory{
+class Memory {
 public:
-    cache L1;
-    cache L2;
+    Cache* L1_cache;
+    Cache* L2_cache;
+    policy cache_policy;
     int access_count_L1 = 0;
     int access_count_L2 = 0;
     int access_count_mem = 0;
-
+    unsigned int dram_cycles;
+    unsigned int L1_cycles;
+    unsigned int L2_cycles;
+    Memory(unsigned MemCyc,unsigned BSize, unsigned L1Size, unsigned L2Size, unsigned L1Assoc,
+           unsigned L2Assoc, unsigned L1Cyc, unsigned L2Cyc, unsigned WrAlloc) : dram_cycles(MemCyc), L1_cycles(L1Cyc),
+           L2_cycles(L2Cyc){ // c'tor
+        // TODO : do pow(2, x) for : block_size, cache_size, associatvie
+        L1_cache = new Cache(pow(2,L1Size), pow(2, BSize), pow(2,L1Assoc));
+        L2_cache = new Cache(pow(2,L2Size), pow(2, BSize), pow(2,L2Assoc));
+    }
+    ~Memory() {
+        delete L1_cache;
+        delete L2_cache;
+    }
 };
