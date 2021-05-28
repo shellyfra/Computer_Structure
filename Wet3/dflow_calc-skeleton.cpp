de04830d6d004ae6e69de2632f8ac6c64ae7cb9f
netabecker <netabec3@gmail.com>
2021-05-28 11:51:41 +0300

    initiallized first function

--- Wet3/dflow_calc-skeleton.cpp
+++ Wet3/dflow_calc-skeleton.cpp
@@ -7,6 +7,7 @@
 #define DST 1
 #define SRC1 2
 #define SRC2 3
+#define MAX_REGS 32
 
 class Node{
 public:
@@ -30,26 +31,58 @@ public:
     Node (Node& other_node) = default;
 };
 
-
 class OOOExe{ //out of order execution
 public:
-    Node* register_array [32];
+    Node* register_array [MAX_REGS];
     std::vector <Node*> inst_array;
     //std::vector <Node*> exit; TODO: maybe add back. check if register_array is sufficient
-    unsigned int op_latency[];
+    unsigned int op_latency[MAX_OPS];
+
+    OOOExe(const unsigned int opsLatency[], const InstInfo progTrace[]) {
+        for (int i = 0; i < MAX_REGS; ++i) {
+            register_array[i] = nullptr;
+        };
+        for (int j = 0; j < MAX_OPS; ++j) {
+            op_latency[j] = opsLatency[j];
+        }
+    }
 
     ~OOOExe(){
         for (int i = 0; i < inst_array.size(); ++i) {
-            delete inst_array.at(i); //TODO: check if it works.
+            delete inst_array.at(i); // TODO: check if it works.
             inst_array.at(i) = nullptr;
         }
     }
     OOOExe(OOOExe& other) = default;
 };
 
+/*  -------------------- END OF CLASSES ---------------------- */
 
 ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[], unsigned int numOfInsts) {
-    return PROG_CTX_NULL;
+    try {
+        OOOExe *out_of_order = new OOOExe(opsLatency, progTrace);
+        //now we will calculate the dependencies :)
+        for (int i = 0; i < numOfInsts; ++i) {
+            Node *new_inst = new Node(opsLatency[i], progTrace[i].opcode, progTrace[i].dstIdx, progTrace[i].src1Idx,
+                                      progTrace[i].src2Idx);
+            out_of_order->inst_array.push_back(new_inst);
+            Node *src1 = out_of_order->register_array[progTrace[i].src1Idx];
+            Node *src2 = out_of_order->register_array[progTrace[i].src2Idx];
+            if (src1 == nullptr && src2 == nullptr) {
+                new_inst->point_to_entry = true;
+            } else if (src1 != nullptr) {
+                new_inst->father1 = src1;
+                new_inst->delay += opsLatency[new_inst->command[SRC1]]; //enlarging the latency field with src1 value
+            } else if (src2 != nullptr) {
+                new_inst->father2 = src2;
+                new_inst->delay += opsLatency[new_inst->command[SRC2]]; //enlarging the latency field with src2 value
+            }
+        }
+        return (ProgCtx) out_of_order; //if the allocation fails, the returned value will be nullptr.
+    }
+    catch(std::bad_alloc& e){ //in case the allocation fails
+        return PROG_CTX_NULL;
+    }
 }
 
 void freeProgCtx(ProgCtx ctx) {
